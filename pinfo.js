5586a13b6d4d1137e9e013f1e0e01f527ab39dae
satyavani <jhansisatya3@gmail.com>
2026-07-11 17:00:19 +0530

    updated website

--- pinfo.js
+++ pinfo.js
@@ -2,9 +2,27 @@ function openProfile(){
             window.location.href="pinfo.html";
         }
 
-
-// Show message when clicking Contact
-function contactMessage() {
+function contactMessage(){
     alert("Thank you for visiting my portfolio! Feel free to contact me.");
 }
+function showDateTime(){
+    const today=new Date();
+    document.getElementById("dateTime").innerHTML =
+        "Today's Date: " + today.toLocaleString();
+}
 
+showDateTime();
+let hour = new Date().getHours();
+
+if(hour < 12){
+    document.getElementById("greeting").innerHTML = "🌞 Good Morning!";
+}
+else if(hour < 18){
+    document.getElementById("greeting").innerHTML = "☀ Good Afternoon!";
+}
+else{
+    document.getElementById("greeting").innerHTML = "🌙 Good Evening!";
+}
+function downloadResume(){
+    window.open("resume.pdf");
+}
