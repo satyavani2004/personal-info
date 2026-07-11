0d35e99b68b395968f5bde5ee7be45104b621587
satyavani <jhansisatya3@gmail.com>
2026-07-11 16:04:07 +0530

    updated website

--- pinfo.js
+++ pinfo.js
@@ -1,3 +1,10 @@
 function openProfile(){
             window.location.href="pinfo.html";
         }
+
+
+// Show message when clicking Contact
+function contactMessage() {
+    alert("Thank you for visiting my portfolio! Feel free to contact me.");
+}
+
